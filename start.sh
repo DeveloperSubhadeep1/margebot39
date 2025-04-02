da35970be3a07e1983a4202808eb763d6f9ae955
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2025-04-02 08:14:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+
+# Set Python virtual environment path
 python=venv/bin/python
+
+# Check if the virtual environment exists, if not, create it
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Upgrade pip & install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run scripts
 $python get_config.py && $python bot.py
