e57457931e03d7f06a58250ca0e523e4b2c6511f
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2025-04-02 08:39:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME","@Developer_Devil_01")
     PASSWORD = os.environ.get("PASSWORD","devil")
     DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://ytclone:ytclone@cluster0.uldqgds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL","-100+2363002982" )  # Add channel id as -100 + Actual ID
+    LOGCHANNEL = os.environ.get("LOGCHANNEL","-1002363002982" )  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
